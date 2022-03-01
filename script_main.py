05859d190974d6603c2d6b0c956a0871f2d504e4
dominikpegler <dinkopeg@posteo.net>
2022-03-01 22:26:08 +0100

    edit

--- script_main.py
+++ script_main.py
@@ -17,6 +17,7 @@ def main():
     scores = cross_val_score(linreg_mod, X, y, cv=cv)
 
     print(f"{scores.mean():.2f} accuracy with a standard deviation of {scores.std():.2f}")
+
     end = time.time()
 
     print(f"\nExecution time: {(end-start):.3f}s")
