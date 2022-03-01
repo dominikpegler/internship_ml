71d68346e538eaff8904bbe6bb94f2b6c6f750bf
dominikpegler <dinkopeg@posteo.net>
2022-03-01 20:58:32 +0100

    edit

---
+++ get_data.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+def get_data():
+    """
+    loads tab-separated file and returns two pandas dataframes for X and y
+    """
+
+    df = pd.read_csv("data",sep='\t',index_col=0)
+    
+    X = df.copy()[[
+        "ffmq_aa1",
+        "ffmq_ds2",
+        "ffmq_aa2",
+        "ffmq_nr2",
+        "ffmq_aa3",
+        "ffmq_nj1",
+        "ffmq_ob1",
+        "ffmq_aa4",
+        "ffmq_nr3",
+        "ffmq_ob2",
+        "ffmq_nr4",
+        "ffmq_nr5",
+        "ffmq_nj2",
+        "ffmq_ob3",
+        "ffmq_ds3",
+        "ffmq_nr6",
+        "ffmq_nj3",
+        "ffmq_ob4", 
+        "ffmq_ds4", 
+        "ffmq_nr7",
+        "ffmq_nj4",
+        "upps_ur_1", 
+        "upps_ur_2", 
+        "upps_ur_3", 
+        "upps_ur_4",
+        "upps_ur_5", 
+        "upps_pm_1", 
+        "upps_pm_2", 
+        "upps_pm_3", 
+        "upps_pm_4",
+        "upps_pm_5",
+        "upps_pe_1", 
+        "upps_pe_2", 
+        "upps_pe_3", 
+        "upps_pe_4",
+        "upps_pe_5", 
+        "upps_ss_1", 
+        "upps_ss_2", 
+        "upps_ss_3", 
+        "upps_ss_4",
+        "upps_ss_5",
+        "dmq_cope_1", 
+        "dmq_cope_2", 
+        "dmq_cope_3", 
+        "dmq_cope_4",
+        "dmq_cope_5",
+        "geschlecht_kod_male",
+        "erwerbstaetig_sub"
+    ]]
+    
+    y = df.copy()["audit"]
+    
+    return X,y
