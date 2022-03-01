606364c8176b434da9e2ff800891f14c3ee4f5b4
dominikpegler <dinkopeg@posteo.net>
2022-03-01 22:23:54 +0100

    edit

--- script_main.py
+++ script_main.py
@@ -1,28 +1,27 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import ShuffleSplit
-from sklearn.model_selection import cross_val_score # alternative: cross_validate
+from sklearn.model_selection import cross_val_score  # alternative: cross_validate
 from sklearn.linear_model import LinearRegression
 from get_data import get_data
 import time
 
+
 def main():
 
     start = time.time()
-    
-    X,y = get_data()    
-    
+
+    X, y = get_data()
+
     linreg_mod = LinearRegression()
-    
+
     cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    scores = cross_val_score(linreg_mod, X, y,cv=cv) 
-                            
-    print("%0.2f accuracy with a standard deviation of %0.2f"
-          % (scores.mean(), scores.std()))
-  
+    scores = cross_val_score(linreg_mod, X, y, cv=cv)
+
+    print(f"{scores.mean():.2f} accuracy with a standard deviation of {scores.std():.2f}")
+
     end = time.time()
-    
-    print(f"\nExecution time: {(end-start).__format__('.3f')}s")
-    
+
+    print(f"\nExecution time: {(end-start):.3f}s")
+
+
 if __name__ == "__main__":
     main()
