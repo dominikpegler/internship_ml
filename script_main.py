53548adf4c490d9ec5270868569e25b0bf7fb171
dominikpegler <dinkopeg@posteo.net>
2022-03-09 20:36:46 +0100

    expanded hyperparamspace

--- script_main.py
+++ script_main.py
@@ -11,20 +11,19 @@ def main():
     start = time.time()
 
     X, y = get_housing_data()
-
-    hyperparams_grid = {"alpha": [1, 10, 50, 100, 150, 175,
-                                  200, 225, 250, 275, 300, 325, 350, 375, 400, 450, 500, 700],
-                        "l1_ratio": [0.1, 0.2, 0.5, 0.8, 0.9]}
-
+    hyperparams_grid = {"alpha": [1, 10, 50, 100, 150, 175, 200, 225, 250, 275,
+                                  300, 325, 350, 375, 400, 450, 500, 700, 1000],
+                        "l1_ratio": [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
+                                     0.8, 0.9, 0.95, 0.99]}
     reg = ElasticNet()
 
-    # Outer CV
+    # outer CV
     outer_cv = GroupShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0)
 
-    # Iterate over main splitter
+    # Iterate over outer CV splitter
     for i_cv, (i_train, i_test) in enumerate(outer_cv.split(X, y, groups=X.index), start=1):
 
         y_train, y_test = split_train_test(y, i_train, i_test)
@@ -34,7 +33,7 @@ def main():
 
         # Nested CV with parameter optimization
         search_reg = RandomizedSearchCV(
-            estimator=reg, n_iter=10, param_distributions=hyperparams_grid, cv=5)
+            estimator=reg, n_iter=100, param_distributions=hyperparams_grid, cv=5)
         result = search_reg.fit(X_train, y_train)
 
         print("best model:", result.best_estimator_)
