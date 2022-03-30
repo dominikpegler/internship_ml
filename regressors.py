31041c879c77b77df4449764b8ad95929f36c5df
dominikpegler <dinkopeg@posteo.net>
2022-03-30 12:13:10 +0200

    write results to csv file

--- regressors.py
+++ regressors.py
@@ -3,18 +3,21 @@ from sklearn.linear_model import ElasticNet
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import GradientBoostingRegressor
+from lightgbm import LGBMRegressor
 
 
 n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-max_features = ['auto', 'sqrt']
+max_features = (1e-2,1e0,"uniform")
 max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
 max_depth.append(None)
-min_samples_split = [2, 5, 10]
-min_samples_leaf = [1, 2, 4]
+min_samples_split = (2,256,"log-uniform")
+min_samples_leaf = (1,256,"log-uniform")
 bootstrap = [True, False]
+learning_rate = (1e-3,1e0,"log-uniform")
+num_leaves = (2,256,"log-uniform")
+min_child_samples = (1,256,"log-uniform")
 
-
-def get_regressor(reg_type="elasticnet"):
+def get_regressor(reg_type="ElasticNet"):
     """
     scikit-learn regressors ready to use for bayesCV
         
@@ -22,20 +25,24 @@ def get_regressor(reg_type="elasticnet"):
     
         Parameters
         ----------
-        reg_type : {'elasticnet', 'rf', 'extratrees', 'gradientboost'} default='elasticnet'
+        reg_type : {'ElasticNet',
+                    'RandomForestRegressor',
+                    'ExtraTreesRegressor',
+                    'GradientBoostingRegressor',
+                    'LGBMRegressor'} default='ElasticNet'
     
     """
     
-    if reg_type == "elasticnet":
+    if reg_type == "ElasticNet":
         reg = ElasticNet()
-        hyperparams_dist = {
+        hyper_space = {
             "alpha": (1e-4,1e3,"log-uniform"),
             "l1_ratio": (1e-3,1.0,"uniform")
             }
 
-    elif reg_type == "rf":
+    elif reg_type == "RandomForestRegressor":
         reg = RandomForestRegressor(random_state = 0)
-        hyperparams_dist = {
+        hyper_space = {
             'n_estimators': n_estimators,
             'max_features': max_features,
             'max_depth': max_depth,
@@ -44,9 +51,9 @@ def get_regressor(reg_type="elasticnet"):
             'bootstrap': bootstrap
         }
 
-    elif reg_type == "extratrees":
+    elif reg_type == "ExtraTreesRegressor":
         reg = ExtraTreesRegressor(random_state = 0)
-        hyperparams_dist = {
+        hyper_space = {
             'n_estimators': n_estimators,
             'max_features': max_features,
             'max_depth': max_depth,
@@ -55,16 +62,46 @@ def get_regressor(reg_type="elasticnet"):
             'bootstrap': bootstrap
         }
         
-    elif reg_type == "gradientboost":
+    elif reg_type == "GradientBoostingRegressor":
         reg = GradientBoostingRegressor(random_state=0)
-        hyperparams_dist = {
+        hyper_space = {
             'n_estimators': n_estimators,
             'max_features': max_features,
             'max_depth': max_depth,
             'min_samples_split': min_samples_split,
             'min_samples_leaf': min_samples_leaf,
-        }        
+        }
+        
+    elif reg_type == "LGBMRegressor":
+        reg = LGBMRegressor(
+                boosting_type='gbdt',
+                num_leaves=31,
+                max_depth=-1,
+                learning_rate=0.1,
+                n_estimators=256,
+                subsample_for_bin=200000,
+                objective=None,
+                class_weight=None,
+                min_split_gain=0.0,
+                min_child_weight=1e-4,
+                min_child_samples=0,
+                subsample=1.0,
+                subsample_freq=0,
+                colsample_bytree=1.0,
+                reg_alpha=0.0,
+                reg_lambda=0.0,
+                random_state=None,
+                n_jobs=1,
+                importance_type='gain',
+                **{'extra_trees': True})
+        
+        hyper_space = {
+            'learning_rate': learning_rate,
+            'num_leaves': num_leaves,
+            'min_child_samples': min_child_samples,
+        }   
+        
     else:
         raise Exception("No valid regressor defined!")
         
-    return reg,hyperparams_dist
+    return reg,hyper_space
--- run_regression.py
+++ run_regression.py
@@ -5,61 +5,102 @@ from regressors import get_regressor
 from utils import split_train_test
 from skopt.plots import plot_objective, plot_evaluations, plot_convergence
 import matplotlib.pyplot as plt
-import numpy as np
 import time
+import traceback
+import csv
+import os
 
 
-def main():
+SIMULATION = False # quick run for testing purposes
+OUTPUT_PATH = "./output/"
+DATA_VARIANT = "complete"
 
-    start = time.time()
+# def main():
+
+start = time.time()
+
+X, y = get_data(DATA_VARIANT)
+
+
+# create directory and report file
+report_dir = OUTPUT_PATH + time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime(start)) + "/"
+os.mkdir(report_dir)
+report_filename = "report.csv"
+with open(report_dir + report_filename , "w", newline="") as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=';')
+    filewriter.writerow(["Type", "Split", "Model", "Train score (R²)", "Test score (R²)", "Finished at timestamp (s)"])
     
 
-    X, y = get_data()
+#reg_type = "LGBMRegressor"
+for reg_type in ["ElasticNet", "LGBMRegressor", "RandomForestRegressor", "GradientBoostingRegressor", "ExtraTreesRegressor"]:
 
-
-    reg_type = "rf"
-    #for reg_type in ["elasticnet", "rf", "extratrees", "gradientboost"]:
-
-    reg,hyperparams_dist = get_regressor(reg_type) # "elasticnet", "rf", "extratrees", "gradientboost"
-
-
-    outer_cv = GroupShuffleSplit(n_splits=5,
+    reg, hyper_space = get_regressor(reg_type) # "elasticnet", "rf", "extratrees", "gradientboost"
+    
+    
+    outer_cv = GroupShuffleSplit(n_splits=5 if SIMULATION==False else 2,
                                  test_size=0.2,
                                  random_state=0
                                 )
-
+    
     # iterate over outer CV splitter
     for i_cv, (i_train, i_test) in enumerate(outer_cv.split(X, y, groups=X.index), start=1):
-
+    
         y_train, y_test = split_train_test(y, i_train, i_test)
         X_train, X_test = split_train_test(X, i_train, i_test)
-
+    
         # nested CV with parameter optimization
         search_reg = BayesSearchCV(
             estimator=reg,
-            search_spaces=hyperparams_dist,
-            n_iter=200,
-            cv=5,
-            n_jobs=8,
+            search_spaces=hyper_space,
+            n_iter=200 if SIMULATION == False else 5,
+            cv=5 if SIMULATION == False else 2,
+            n_jobs=-2,
             random_state=0
         )
-
+    
         result = search_reg.fit(X_train, y_train.values.ravel())
+        
+        # write results to report file
+        with open(report_dir + report_filename, "a", newline="") as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=';')
+            filewriter.writerow(
+                (
+                    reg_type, 
+                    i_cv, 
+                    result.best_estimator_, 
+                    result.score(X_train, y_train), 
+                    result.score(X_test, y_test), 
+                    time.time()-start,
+                    )
+                )
+    
+    # create plots
+    try:
+        plot_convergence(search_reg.optimizer_results_)
+        plt.savefig(report_dir + reg_type + "_plot_convergence.png")
+    except:
+        with open(report_dir + reg_type + "_plot_convergence.log", "w") as fo:
+            fo.write(traceback.format_exc())
+    
+    try:
+        plot_evaluations(search_reg.optimizer_results_[0])
+        plt.savefig(report_dir + reg_type + "_plot_evaluations.png")
+    except:
+        with open(report_dir + reg_type + "_plot_evaluations.log", "w") as fo:
+            fo.write(traceback.format_exc())
+    
+    try:
+        plot_objective(search_reg.optimizer_results_[0])
+        plt.savefig(report_dir + reg_type + "_plot_objective.png")
+    except:
+        with open(report_dir + reg_type + "_plot_objective.log", "w") as fo:
+            fo.write(traceback.format_exc())
+        
 
-        print(f"Split {i_cv}:", result.best_estimator_)
-        print("train score:", round(result.score(X_train, y_train), 5))
-        print("test  score:", round(result.score(X_test, y_test), 5))
-        print("\n")
-
-    plot_convergence(search_reg.optimizer_results_)
-    plt.show()
-    plot_evaluations(search_reg.optimizer_results_[0])
-    plt.show()
-    plot_objective(search_reg.optimizer_results_[0])
-    plt.show()
-
-    print(f"Execution time: {(time.time()-start):.3f}s")
 
 
-if __name__ == "__main__":
-    main()
+print(f"Overall execution time: {(time.time()-start):.3f}s")
+
+
+# if __name__ == "__main__":
+#     main()
